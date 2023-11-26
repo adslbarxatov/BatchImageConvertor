e6b317c72a356b02223500101df1fd92382631a9
Nicolay B. aka RD_AAOW <adslbarxatov@gmail.com>
2023-11-26 22:32:17 +1100

    Assembly v 3.7 (26.11.23)

--- src/JP2.h
+++
@@ -1,8 +0,0 @@
-// Спецификации для JPEG2000
-#define OPJ_EXPORTS
-#include "..\JP2\openjpeg.h"
-#pragma comment (lib, "JP2\\Release\\OpenJP2.lib")
-
-// Декларации
-sint JP2_LoadImage (schar *FileName, uint *Width, uint *Height, uchar **Buffer);
-sint JP2_SaveImage (schar *FileName, uint Width, uint Height, uchar *Buffer, uchar CodecType);/**/
